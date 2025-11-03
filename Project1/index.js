2a913283aa38aa1790d5495832ba1328c8127baa
TahaZ4 <abdullahtaha5818@gmail.com>
2025-11-03 13:04:57 +0200

    Update index.js

--- Project1/index.js
+++ Project1/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const cors = require('cors'); //1
+const dotenv = require('dotenv'); //2
+const tripRouter = require('./routes/tripRoutes.js');
+const userRouter = require('.routes/userRoutes.js');
+const authRouter = require('.routes/authRoutes.js') //3
+const app = express():
+//Load environment variables from .env file
+dotenv.config(); //4
+app.use(cors()); //5
+
+// Create an instance of the Express application
 const app = express();
-const TripRouter=require('./routes/TripRouter');
-
+// Use middleware to parse JSON data from request bodies
 app.use(express.json());
-app.use('/api/v1/trips', TripRouter);
-
-module.exports=
-{
-    app
+app.use('/trips', tripRouter);
+app.use(/'users', userRouter);
+app.use('/auth',aythRouter); //5
+module.exports = {
+  app,
 };
