10d61f236b7c72d5c328959b641f9d6c80c83520
TahaZ4 <abdullahtaha5818@gmail.com>
2025-11-04 12:59:56 +0200

    Update userRouter.js

--- Project1/userRouter.js
+++ Project1/userRouter.js
@@ -2,14 +2,14 @@ userRouter.use(verifyAdmin)
 
 // All USERS
 userRouter
-.route('/')
+  .route('/')
   .get(tretrieveAllusers) //Get users
   .post(createuser);
 
 userRouter
   .route('/:id')
   .get(retrieveUserById)
-  .put(UpdateUserById
+  .put(UpdateUserById)
        .delete(deletetUserbyId);
 
 module.exports = userRouter;
